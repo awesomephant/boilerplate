af13508c776f82433585a0a825684a1cbe32f364
Max Köhler <max@koehler-kn.de>
2014-11-02 18:50:59 +0100

    cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -75,15 +75,15 @@ module.exports = function (grunt) {
         }
 	});
 	
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	
-	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks(
+		'grunt-contrib-watch',
+		'grunt-contrib-sass',
+		'grunt-contrib-jshint',
+		'grunt-contrib-uglify',
+		'grunt-contrib-cssmin',
+		'grunt-autoprefixer',
+		'grunt-browser-sync'
+	);
 
 
 	grunt.registerTask('css', ['sass', 'autoprefixer', 'cssmin']);
